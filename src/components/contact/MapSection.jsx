b5047bbc0949ad0b2ef0b25d678f72de563c1c99
Hlamong55 <bijoymarma55@gmail.com>
2026-02-17 23:49:59 +0600

    map location added in contact section

--- src/components/contact/MapSection.jsx
+++ src/components/contact/MapSection.jsx
@@ -1,14 +1,18 @@
 const MapSection = () => {
   return (
-    <div className="rounded-2xl overflow-hidden shadow-lg border">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.902291236999!2d90.39158531537333!3d23.75091258459045"
-        width="100%"
-        height="350"
-        allowFullScreen=""
-        loading="lazy"
-      ></iframe>
-    </div>
+    <section className="space-y-4">
+      <h2 className="text-2xl font-bold text-center">Our Location</h2>
+
+      <div className="rounded-2xl overflow-hidden shadow-xl border bg-base-100">
+        <iframe
+          src="https://www.google.com/maps?q=Chattogram%20Bangladesh&output=embed"
+          width="100%"
+          height="350"
+          loading="lazy"
+          className="border-0"
+        ></iframe>
+      </div>
+    </section>
   );
 };
 
